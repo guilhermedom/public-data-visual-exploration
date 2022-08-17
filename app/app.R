3e0544dd922e26d63923a3f853acfbfe1aec9da2
guilhermedom <guilhermedfs@gmail.com>
2022-08-16 22:38:44 -0300

    Change technical term in UI
    Exchange "investment" with the proper "committed cost" term, as it is a more technically sound name for the type of public expenditure.

--- app/app.R
+++ app/app.R
@@ -2,30 +2,32 @@ library(shiny)
 library(shinythemes)
 library(RColorBrewer)
 
-data = read.csv("../data/raw/gdp_investment_data.csv", sep = ";")
+data = read.csv("gdp_committed_cost_data.csv", sep = ";")
 
 # Define UI for application
 ui = fluidPage(
     theme = shinytheme("paper"),
     
     # Application title
-    titlePanel("Visually Exploring Brazilian Cities GDPs and Public Investments"),
+    titlePanel("Visually Exploring Brazilian Cities' Gross Domestic Products (GDPs) and Committed Costs"),
+    helpText("Committed costs are budgeted payments to be made in the future for
+             goods or services yet to be taken."),
     
     fluidRow(
-        column(2, align = "center", h3(textOutput("textInvestmentID")),
-               tableOutput("tableInvestmentID")),
-        column(4, plotOutput("histInvestmentID")),
+        column(2, align = "center", h3(textOutput("textCostID")),
+               tableOutput("tableCostID")),
+        column(4, plotOutput("histCostID")),
         column(2, align = "center", h3(textOutput("textGDPID")),
                tableOutput("tableGDPID")),
         column(4, plotOutput("histGDPID"))
     ),
     fluidRow(
-        column(3, plotOutput("boxplotInvestmentID")),
+        column(3, plotOutput("boxplotCostID")),
         column(3, plotOutput("boxplotGDPID")),
         column(6, plotOutput("scatterPlotID"))
     ),
     fluidRow(
-        column(4, plotOutput("largestInvestmentID")),
+        column(4, plotOutput("largestCostID")),
         column(4, plotOutput("largestGDPID")),
         column(4, plotOutput("largestProportionID"))
     )
@@ -33,21 +35,21 @@ ui = fluidPage(
 
 # Define server logic
 server = function(input, output) {
-    output$textInvestmentID = renderText({"Investment Descriptive Stats"})
-    output$tableInvestmentID = renderTable({as.array(summary(data$INVESTMENT))})
+    output$textCostID = renderText({"Committed Cost Descriptive Stats"})
+    output$tableCostID = renderTable({as.array(summary(data$COMMITTEDCOST))})
     output$textGDPID = renderText({"GDP Descriptive Stats"})
     output$tableGDPID = renderTable({as.array(summary(data$GDP))})
     
-    output$histInvestmentID = renderPlot({
-        hist(data$INVESTMENT, main = "Investment Values", col = brewer.pal(n = 3, name = "Paired"), xlab = "Investment")
+    output$histCostID = renderPlot({
+        hist(data$COMMITTEDCOST, main = "Committed Cost Values", col = brewer.pal(n = 3, name = "Paired"), xlab = "Committed Cost")
     })
     
     output$histGDPID = renderPlot({
         hist(data$GDP, main = "GDP Values", col = brewer.pal(n = 3, name = "Pastel1"), xlab = "GDP")
     })
     
-    output$boxplotInvestmentID = renderPlot({
-        boxplot(data$INVESTMENT, main = "Investment Values", col = brewer.pal(n = 3, name = "Paired"), outline = F, horizontal = T)
+    output$boxplotCostID = renderPlot({
+        boxplot(data$COMMITTEDCOST, main = "Committed Cost Values", col = brewer.pal(n = 3, name = "Paired"), outline = F, horizontal = T)
     })
     
     output$boxplotGDPID = renderPlot({
@@ -55,13 +57,13 @@ server = function(input, output) {
     })
     
     output$scatterPlotID = renderPlot({
-        plot(data$INVESTMENT, data$GDP, main = "Investment vs. GDP", xlab = "Investment", ylab = "GDP", pch = 19, col = "blue")
+        plot(data$COMMITTEDCOST, data$GDP, main = "Committed Cost vs. GDP", xlab = "Committed Cost", ylab = "GDP", pch = 19, col = "blue")
     })
     
-    topInvestments = head(data[order(-data$INVESTMENT),], 10)
-    output$largestInvestmentID = renderPlot({
-        barplot(topInvestments$INVESTMENT, col = brewer.pal(n = 10, name = "RdBu"), las = 2, main = "Largest Investments", xlab = "Investments")
-        legend("topright", legend = topInvestments$CITY, col = brewer.pal(n = 10, name = "RdBu"), cex = 0.8, ncol = 2, lwd = 4)
+    topCosts = head(data[order(-data$COMMITTEDCOST),], 10)
+    output$largestCostID = renderPlot({
+        barplot(topCosts$COMMITTEDCOST, col = brewer.pal(n = 10, name = "RdBu"), las = 2, main = "Largest Committed Costs", xlab = "Committed Cost")
+        legend("topright", legend = topCosts$CITY, col = brewer.pal(n = 10, name = "RdBu"), cex = 0.8, ncol = 2, lwd = 4)
         box()
     })
     
@@ -70,10 +72,10 @@ server = function(input, output) {
         pie(topGDPs$GDP, col = brewer.pal(n = 10, name = "Spectral"), labels = topGDPs$CITY, main = "Largest GDPs", xlab = "GDPs")
     })
     
-    data$PROPORTION = data$INVESTMENT / data$GDP
+    data$PROPORTION = data$COMMITTEDCOST / data$GDP
     topProportions = head(data[order(-data$PROPORTION), ], 10)
     output$largestProportionID = renderPlot({
-        barplot(topProportions$PROPORTION, col = brewer.pal(n = 10, name = "Set3"), las = 2, main = "Largest Investments/GDP Ratio", xlab = "Proportion")
+        barplot(topProportions$PROPORTION, col = brewer.pal(n = 10, name = "Set3"), las = 2, main = "Largest Committed Cost/GDP Ratio", xlab = "Proportion")
         legend("topright", legend = topProportions$CITY, col = brewer.pal(n = 10, name = "Set3"), cex = 0.8, ncol = 2, lwd = 4)
         box()
     })
